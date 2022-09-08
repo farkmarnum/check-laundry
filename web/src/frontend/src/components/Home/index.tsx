35dee2452bcdafa33b6e95a284ad9612881bebf5
farkmarnum <mark.farnum@gmail.com>
2022-09-08 01:41:06 -0400

    Refactor

--- web/src/frontend/src/components/Home/index.tsx
+++ web/src/frontend/src/components/Home/index.tsx
@@ -1,16 +1,48 @@
-import { useEffect } from 'preact/hooks';
+import { useEffect, useState } from 'preact/hooks';
+import Spinner from '../Spinner';
+
 import s from './style.css';
 
+const STATION_ID = 'basement'; // For now, just one station.
+
+const Loading = () => (
+  <div>
+    <div>Loading laundry data...</div>
+    <div style="margin-top: -6px">
+      <Spinner />
+    </div>
+  </div>
+);
+
 const Home = () => {
+  const [stationData, setStationData] = useState();
+  const [error, setError] = useState('');
+
   useEffect(() => {
-    document.title = 'Check Laundry';
+    (async () => {
+      try {
+        const resp = await fetch(
+          `https://checklaundry.com/api/v1/stationData/${STATION_ID}`,
+        );
+
+        const { unitStates } = (await resp.json()) || {};
+        if (unitStates) {
+          setStationData(unitStates);
+        }
+      } catch (err) {
+        console.error(err);
+        setError('Failed to fetch data 😕');
+      }
+    })();
   }, []);
 
   return (
     <div>
       <h1>Check Laundry 🧺</h1>
-      <div className={s.stations}>
-        <a href="/stations/90_7th_ave">90 7th Ave</a>
+      <div className={s.units}>
+        {stationData && JSON.stringify(stationData)}
+        {error && error}
+        {!stationData && !error && <Loading />}
       </div>
     </div>
   );
--- web/src/frontend/src/index.tsx
+++ web/src/frontend/src/index.tsx
@@ -1,17 +1,5 @@
-import { Route, Router } from 'preact-router';
-import { createHashHistory } from 'history';
-
 import Home from './components/Home';
-import Station from './components/Station';
 
 import './style';
 
-const Root = () => (
-  // @see https://github.com/preactjs/preact-router/issues/423
-  <Router history={createHashHistory() as any}>
-    <Route path="/" component={Home} />
-    <Route path="/stations/:stationId" component={Station} />
-  </Router>
-);
-
-export default Root;
+export default Home;
